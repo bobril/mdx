25d88829b6f8b8439cc8aef717556af3d930eafa
Boris Letocha <boris.letocha@gmail.com>
2021-04-17 22:08:38 +0200

    Rename extension to mdxb

--- mdx.d.ts
+++ mdx.d.ts
@@ -1,5 +1,14 @@
-declare module "*.mdx" {
+declare module "*.mdxb" {
     export const metadata: Record<string, any>;
     const content: import("bobril").IComponentFactory<Record<string, any>>;
+    export default content;
+}
+
+declare module "mdxb:*" {
+    const content: Array<{
+        name: string;
+        metadata: Record<string, any>;
+        factory: Promise<import("bobril").IComponentFactory<Record<string, any>>>;
+    }>;
     export default content;
 }
